a245723b06d4faae0a54bc26e40b6c69fe4396e0
Truong Nguyen <vonguyen.0407@gmail.com>
2025-04-18 09:52:26 +0700

    load gio hang, chuc nang xoa, xoa tat ca (CartActivity,CartEntryAdapter), them phuong thuc trong CartDao

--- app/src/main/java/com/ctut/mart4u/customer/CartActivity.java
+++ app/src/main/java/com/ctut/mart4u/customer/CartActivity.java
@@ -1,26 +1,62 @@
 package com.ctut.mart4u.customer;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.ctut.mart4u.BaseActivity;
 import com.ctut.mart4u.R;
+import com.ctut.mart4u.customer.adapter.CartEntryAdapter;
+import com.ctut.mart4u.db.DatabaseHelper;
+import com.ctut.mart4u.model.CartDetail;
 
-public class CartActivity extends AppCompatActivity {
+import java.util.List;
+
+public class CartActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.customer_cart;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cart);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        int userId = getIntent().getIntExtra("user_id", -1);
+        Toast.makeText(this, "User ID: " + userId, Toast.LENGTH_SHORT).show();
+
+        RecyclerView recyclerViewCart = findViewById(R.id.recyclerViewCart);
+        recyclerViewCart.setLayoutManager(new LinearLayoutManager(this));
+        //lay du lieu tu database
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(this);
+        List<CartDetail> cartList = databaseHelper.getCartDetailDao().getCartDetailsByUser(userId);
+
+//        Toast.makeText(this, "Cart List Size: " + cartList.size(), Toast.LENGTH_SHORT).show();
+
+        // su kien click vao nut xoa tat ca
+        Button btnDeleteAll = findViewById(R.id.btnDeleteAllCart);
+        btnDeleteAll.setOnClickListener(v -> {
+            databaseHelper.getCartDetailDao().deleteAll(userId);
+            cartList.clear();
+            recyclerViewCart.getAdapter().notifyDataSetChanged();
+            Toast.makeText(this, "Đã xóa tất cả sản phẩm khỏi giỏ hàng", Toast.LENGTH_SHORT).show();
         });
+
+
+        //set adapter
+        CartEntryAdapter adapter = new CartEntryAdapter(this, cartList);
+        recyclerViewCart.setAdapter(adapter);
+
+        EdgeToEdge.enable(this);
+
     }
 }
