fe233b3af7defd2afce0aef23785833465fb90ea
DatOris <ntdathttt2211003@student.ctuet.edu.vn>
2025-04-19 13:37:56 +0700

    chi tiết đơn hàng

--- app/src/main/java/com/ctut/mart4u/customer/HistoryActivity.java
+++ app/src/main/java/com/ctut/mart4u/customer/HistoryActivity.java
@@ -16,7 +16,7 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.customer_orders_history);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
